4bf129374449f52356601c2f70b31809b964671d
benny999 <benny999.yu@gmail.com>
2024-09-25 18:47:18 +0800

    testing auto tab with expandtabs

--- week-02/stack.mjs
+++ week-02/stack.mjs
@@ -1,55 +1,55 @@
 export default class Stack {
-	// member varibles :
-	// arr  : the implementation of stack
-	// top_index  :  the index of current top element
+    // member varibles :
+    // arr  : the implementation of stack
+    // top_index  :  the index of current top element
 
-	constructor() {
-		this.arr = [];
-	}
+    constructor() {
+        this.arr = [];
+    }
 
-	// push element into stack
-	push(element) {
-		this.arr.push(element);
-	}
+    // push element into stack
+    push(element) {
+        this.arr.push(element);
+    }
 
-	// pop elements at the top and return the value
-	pop() {
-		return this.arr.pop();
-	}
+    // pop elements at the top and return the value
+    pop() {
+        return this.arr.pop();
+    }
 
-	// only returning top element
-	peek() {
-		return this.arr.at(this.arr.length - 1);
-	}
+    // only returning top element
+    peek() {
+        return this.arr.at(this.arr.length - 1);
+    }
 
-	// check if it's empty
-	isEmpty() {
-		if(this.arr.length == 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    // check if it's empty
+    isEmpty() {
+        if(this.arr.length == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	// return the number of elements
-	size() {	
-		return this.arr.length;
-	}
+    // return the number of elements
+    size() {	
+        return this.arr.length;
+    }
 
-	// clear all elements in stack
-	clear() {
-		this.arr.length = 0;
-	}
+    // clear all elements in stack
+    clear() {
+        this.arr.length = 0;
+    }
 
-	// print all elements in stack
-	print() {
-		if(this.arr.length == 0) {
-			console.log();
-		} else {
-			for(let i = this.arr.length - 1; i >= 0; i --) {
-				console.log(this.arr[i]);
-			}
-		}
+    // print all elements in stack
+    print() {
+        if(this.arr.length == 0) {
+            console.log();
+        } else {
+            for(let i = this.arr.length - 1; i >= 0; i --) {
+                console.log(this.arr[i]);
+            }
+        }
 
-	}
+    }
 }
